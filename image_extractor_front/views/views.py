b87f49f84efc7ed254e06fea1f676e104c8f40dd
ForestParadiseOrigen <andres.jcv.678@gmail.com>
2024-05-29 11:11:15 -0500

    Refactor - Se ha refactorizado el método upload_view.
    Desde ahora el encargado de realizar el almacenamiento, eliminación y modificación de los archivos es el backend y no el frontend. Desde ahora, solamente se van a llamar los métodos necesarios desde el front.

--- image_extractor_front/views/views.py
+++ image_extractor_front/views/views.py
@@ -1,16 +1,40 @@
 import os
-from django.http import JsonResponse, HttpResponseRedirect
+
+from django.http import JsonResponse
 from django.shortcuts import render
-from image_extractor import settings
-from ..forms.upload_form import UploadForm
-from image_extractor_back.utils.process_pdf_file import extract_images_from_pdf
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import default_storage
 
+from ..forms.upload_form import UploadForm
+
+from image_extractor import settings
+from image_extractor_back.utils.save_new_file import save_new_file
+from image_extractor_back.utils.process_pdf_file import extract_images_from_pdf
+from image_extractor_back.utils.del_new_file import del_new_file
 
 @csrf_exempt
 def upload_view(request):
-    
+
+    # Explicación: Por medio de esta sección podemos comenzar con el proceso de extracción normalmente.
+    if request.method == "POST":
+        
+        # Encargado de comprobar que el archivo que llega en el requerimiento del formulario contiene un archivo PDF.
+        form = UploadForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            # Método encargado de guardar los archivos en el backend del proyecto.
+            save_file = save_new_file(request)  
+            return save_file
+        else:
+            print("Al parecer el archivo no parece ser un PDF.")     
+    else:
+        form = UploadForm()
+        
+    return render(request, "upload_template.html", {"form": form})
+
+
+''' 
+    # Sección: En caso de que nuestro código funcione correctamente hay que eliminar este bloque de comentarios.
     # Explicación: Se encarga de guardar los archivos que llegan por mediod del request.
     def save_new_file(request):
         uploaded_file = request.FILES["file"]
@@ -35,19 +59,4 @@ def upload_view(request):
                 print("Error al eliminar el archivo.")
         else:
             print("No se encontró ningún archivo para eliminar.")
-
-    # Explicación: Por medio de esta sección podemos comenzar con el proceso de extracción normalmente.
-    if request.method == "POST":
-    
-        form = UploadForm(request.POST, request.FILES)
-        
-        if form.is_valid():
-            return save_new_file(request)
-        else:
-            print("Al parecer el archivo no parece ser un PDF.")     
-    else:
-        form = UploadForm()
-        
-    return render(request, "upload_template.html", {"form": form})
-
-    
+'''
